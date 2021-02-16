7b3018eabf381c11dfc6e900b1dafc8ac963522a
Blindrabit <a.r.tucker@hotmail.co.uk>
2021-02-16 13:46:15 +0000

    rm dummy .env and added local host to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY',  default='dummy_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1' ]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
